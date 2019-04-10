bf9146dfcea1197fec3668f5e79cb7ffc772bc6a
Tinybo <1069792235@qq.com>
2019-04-10 20:56:45 +0800

    完成用户表模型映射。

---
+++ config/tableModal/user.js
@@ -0,0 +1,121 @@
+const Sequelize = require('sequelize');         // 导入ORM框架
+const config = require('../mysql');       // 导入数据库配置
+
+// 创建sequelize(ORM)实例
+let sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 30000
+        }
+    }
+);
+
+// 学生用户表映射模型
+const Student = sequelize.define('student', {
+    stu_id: { type: Sequelize.STRING, primaryKey: true },
+    stu_name: Sequelize.STRING,
+    num: Sequelize.STRING,
+    sex: Sequelize.STRING,
+    age: Sequelize.BIGINT,
+    college: Sequelize.STRING,
+    department: Sequelize.STRING,
+    major: Sequelize.STRING,
+    class: Sequelize.STRING,
+    grade: Sequelize.STRING,
+    position: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    qq: Sequelize.STRING,
+    create_time: Sequelize.DATE,
+    new_time: Sequelize.DATE,
+    total: Sequelize.BIGINT,
+    type: Sequelize.BIGINT,
+    isFinish: Sequelize.BIGINT,
+    password: Sequelize.STRING
+}, {
+    timestamps: false,
+    freezeTableName: true
+}, function(require, factory) {
+    'use strict';
+    console.log('ORM已完成！');
+});
+
+// 教师用户表映射模型
+const  Teacher = sequelize.define('teacher', {
+    tea_id: { type: Sequelize.STRING, primaryKey: true },
+    tea_name: Sequelize.STRING,
+    num: Sequelize.STRING,
+    sex: Sequelize.STRING,
+    age: Sequelize.BIGINT,
+    college: Sequelize.STRING,
+    department: Sequelize.STRING,
+    position: Sequelize.STRING,
+    create_time: Sequelize.DATE,
+    new_time: Sequelize.DATE,
+    total: Sequelize.BIGINT,
+    type: Sequelize.BIGINT,
+    isFinish: Sequelize.BIGINT,
+    phone: Sequelize.STRING,
+    password: Sequelize.STRING
+}, {
+    timestamps: false,
+    freezeTableName: true
+}, function(require, factory) {
+    'use strict';
+    console.log('ORM已完成！');
+});
+
+// 学工办用户表映射模型
+const  Office = sequelize.define('office', {
+    off_id: { type: Sequelize.STRING, primaryKey: true },
+    off_name: Sequelize.STRING,
+    num: Sequelize.STRING,
+    sex: Sequelize.STRING,
+    college: Sequelize.STRING,
+    department: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    create_time: Sequelize.DATE,
+    new_time: Sequelize.DATE,
+    type: Sequelize.BIGINT,
+    isFinish: Sequelize.BIGINT,
+    password: Sequelize.STRING
+}, {
+    timestamps: false,
+    freezeTableName: true
+}, function(require, factory) {
+    'use strict';
+    console.log('ORM已完成！');
+});
+
+// 学院领导用户表映射模型
+const Leader = sequelize.define('leader', {
+    lea_id: { type: Sequelize.STRING, primaryKey: true },
+    lea_name: Sequelize.STRING,
+    college: Sequelize.STRING,
+    department: Sequelize.STRING,
+    num: Sequelize.STRING,
+    sex: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    type: Sequelize.BIGINT,
+    isFinish: Sequelize.BIGINT,
+    password: Sequelize.STRING
+}, {
+    timestamps: false,
+    freezeTableName: true
+}, function(require, factory) {
+    'use strict';
+    console.log('ORM已完成！');
+});
+
+module.exports = {
+    Student,
+    Teacher,
+    Office,
+    Leader
+}
