82b019e17fcad1335e6147e356a11b395fe6aa6f
Tinybo <1069792235@qq.com>
2019-04-14 23:03:00 +0800

    增加请假表模型。

---
+++ config/tableModal/leave.js
@@ -0,0 +1,79 @@
+const Sequelize = require('sequelize');         // 导入ORM框架
+const config = require('../mysql');       // 导入数据库配置
+
+// 创建sequelize(ORM)实例
+let sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    {
+        host: config.host,
+        dialect: 'mysql',
+        pool: {
+            max: 5,
+            min: 0,
+            idle: 30000
+        }
+    }
+);
+
+// 学生用户表映射模型
+const StudentLeave = sequelize.define('student_leave', {
+    id: { type: Sequelize.STRING, primaryKey: true },
+    name: Sequelize.STRING,
+    num: Sequelize.STRING,
+    college: Sequelize.STRING,
+    department: Sequelize.STRING,
+    major: Sequelize.STRING,
+    class: Sequelize.STRING,
+    grade: Sequelize.STRING,
+    position: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    qq: Sequelize.STRING,
+    startTime: Sequelize.DATE,
+    endTime: Sequelize.DATE,
+    createTime: Sequelize.DATE,
+    total: Sequelize.BIGINT,
+    off_opinion: Sequelize.BIGINT,
+    department_opinion: Sequelize.BIGINT,
+    note: Sequelize.STRING,
+    cancel_leave: Sequelize.BIGINT,
+    reason: Sequelize.STRING,
+}, {
+    timestamps: false,
+    freezeTableName: true
+}, function(require, factory) {
+    'use strict';
+    console.log('ORM已完成！');
+});
+
+// 教师用户表映射模型
+const TeacherLeave = sequelize.define('teacher_leave', {
+    id: { type: Sequelize.STRING, primaryKey: true },
+    name: Sequelize.STRING,
+    num: Sequelize.STRING,
+    college: Sequelize.STRING,
+    department: Sequelize.STRING,
+    position: Sequelize.STRING,
+    phone: Sequelize.STRING,
+    startTime: Sequelize.DATE,
+    endTime: Sequelize.DATE,
+    createTime: Sequelize.DATE,
+    total: Sequelize.BIGINT,
+    off_opinion: Sequelize.BIGINT,
+    department_opinion: Sequelize.BIGINT,
+    note: Sequelize.STRING,
+    cancel_leave: Sequelize.BIGINT,
+    reason: Sequelize.STRING,
+}, {
+    timestamps: false,
+    freezeTableName: true
+}, function(require, factory) {
+    'use strict';
+    console.log('ORM已完成！');
+});
+
+module.exports = {
+    StudentLeave,
+    TeacherLeave,
+}
