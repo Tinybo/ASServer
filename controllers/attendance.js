2b2dd5ba2fc73df48f352f9f29c224196e6b616f
Tinybo <1069792235@qq.com>
2019-04-21 13:30:18 +0800

    增加所有课程搜索接口。

--- controllers/attendance.js
+++ controllers/attendance.js
@@ -1,4 +1,6 @@
 const { CourseParent, CourseChild } = require('../config/tableModal/attendance');    // 导入请假表模型
+const { Student } = require('../config/tableModal/user');    // 导入请假表模型
+
 const { parsePostData } = require('../utils/handlePost');    // 导入 POST 请求数据处理函数
 
 /**
@@ -31,7 +33,7 @@ async function createCourse (ctx, next) {
             position: postData.position,
             phone: postData.phone,
             createTime: dateTime + ' ' + momentTime,
-            address: postData.addrss,
+            address: postData.address,
             isFinish: 0
         });
         
@@ -102,7 +104,142 @@ async function searchCourse (ctx, next) {
     await next();
 }
 
+/**
+ * 完成签到操作。
+ * @author Tinybo
+ * @date 2019 04 20
+ * @param {*} ctx 上下文对象
+ * @param {*} next 程序控制对象
+ */
+async function signIn (ctx, next) {
+    ctx.response.type = 'json';                 // 设置数据返回格式
+    let postData = await parsePostData(ctx);    // 获取请求数据
+    let createTime = new Date();                // 创建注册时间
+    let dateTime = createTime.toLocaleDateString();
+    let momentTime = createTime.toLocaleTimeString();
+
+    console.log('请求参数', postData); // 输出接收到的请假条信息
+
+    // 编写查询语句
+    try {
+        // 查找同专业、同年级、同班级的所有学生
+        await Student.findAll({
+            where: {
+                college: postData.college,
+                department: postData.department,
+                major: postData.major,
+                grade: postData.grade,
+                class: postData.class
+            }
+        }).then((data) => {
+            if (data[0]) {
+                console.log('已经成功找到所有学生。');
+                let should_num = data.length;
+
+                ctx.response.body = {
+                    code: '200',
+                    data: data
+                };
+            } else {
+                console.log('该专业，年级，班级的同学没有！');
+                ctx.response.body = {
+                    code: '404',
+                    msg: '该专业，年级，班级的同学没有！'
+                };
+            } 
+        }).catch((error) => {
+            ctx.response.body = {
+                code: '404',
+                msg: '错误原因：' + error
+            };
+        });
+    } catch (error) {
+        ctx.response.body = {
+            code: '404',
+            msg: '错误原因：' + error
+        };
+    }
+
+    return;
+    await CourseParent.findAll({
+        where: {
+            id: postData.courseId
+        }
+    }).then((data) => {
+        if (data[0]) {
+            console.log('已经成功找到该课堂。');
+            ctx.response.body = {
+                code: '200',
+                data: data[0]
+            };
+        } else {
+            console.log('该用户不存在。');
+            ctx.response.body = {
+                code: '404',
+                msg: '该课堂不存在！'
+            };
+        } 
+    }).catch((error) => {
+        console.log('出错了：', error);
+    });
+
+    await next();
+}
+
+/**
+ * 完成获取所有课堂操作。
+ * @author Tinybo
+ * @date 2019 04 21
+ * @param {*} ctx 上下文对象
+ * @param {*} next 程序控制对象
+ */
+async function getAllCourse (ctx, next) {
+    ctx.response.type = 'json';                 // 设置数据返回格式
+    let postData = await parsePostData(ctx);    // 获取请求数据
+
+    console.log('请求参数', postData); // 输出接收到的请假条信息
+
+    // 编写查询语句
+    try {
+        // 根据教师ID查找所有课程
+        await CourseParent.findAll({
+            where: {
+                tea_id: postData.tea_id
+            }
+        }).then((data) => {
+            if (data[0]) {
+                console.log('已经成功找到所有课程。');
+
+                ctx.response.body = {
+                    code: '200',
+                    data: data
+                };
+            } else {
+                console.log('该课程不存在！');
+                ctx.response.body = {
+                    code: '404',
+                    msg: '无课程！'
+                };
+            } 
+        }).catch((error) => {
+            ctx.response.body = {
+                code: '404',
+                msg: '错误原因：' + error
+            };
+        });
+    } catch (error) {
+        ctx.response.body = {
+            code: '404',
+            msg: '错误原因：' + error
+        };
+    }
+
+    await next();
+}
+
 module.exports = {
     'POST /createCourse': createCourse,
-    'POST /searchCourse': searchCourse
+    'POST /searchCourse': searchCourse,
+    'POST /signIn': signIn,
+    'POST /getAllCourse': getAllCourse
 };
